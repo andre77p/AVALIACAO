9ab9743f5e78129cee264f6fb4c419215a4dd6f3
andre pereira rafael <andre.p.rafael2018@gmail.com>
2022-10-26 00:03:06 -0300

    fish

--- src/pages/juros/index.js
+++ src/pages/juros/index.js
@@ -1,28 +1,48 @@
-import { useEffect, useSatate } from "react"
+import { useEffect, useState } from "react"
 
 export default function Index() {
 
-    const [entrada, setEntrado] = useState():
+    const [entrada, setEntrada] = useState();
     const [capital, setCapital] = useState();
     const [taxa, setTaxa] = useState();
     const [periodo, setPeriodo] = useState();
     const [resposta, setResposta] = useState([]);
+
+    function jurosCompostos (entrada, capital, taxa, periodo) {
+        try {
+            let ca = capital - entrada;
+            let ma = ca*Math.pow(1 + taxa/100, periodo);
+            let ju = ma - ca;
+            let pe = ma/(periodo*12);
+            let array = `${ju.toFixed(2).replace('.', ',')}, ${pe.toFixed(2).replace('.', ',')}, ${ma.toFixed(2).replace('.',',')}`;
+
+            return array;
+
+        } catch (err) {
+            return err.message
+        }
+    }
 
     function Juros () {
         const resp = jurosCompostos(entrada, capital, taxa, periodo);
         setResposta(resp);
         console.log(resp)
     } 
- }
 
-
+    
  return (
     <div>
         <h3>Juros compostos</h3>
-        <input type="number" value={capital} onChange={e => setCapital( Number(e.target.value) )}/>
-        <input type="number" value={entrada} onChange={e => setEntrada( Number(e.target.value) )}/>
-        <input type="number" value={taxa} onChange={e => setTaxa( Number(e.target.value) )}/>
-        <input type="number" value={periodo} onChange={e => setPeriodo( Number(e.target.value) )}/>
+        <p>Capital</p><input type="number" value={capital} onChange={e => setCapital( Number(e.target.value) )}/>
+        <p>Entrada</p><input type="number" value={entrada} onChange={e => setEntrada( Number(e.target.value) )}/>
+        <p>Taxa(anual)</p><input type="number" value={taxa} onChange={e => setTaxa( Number(e.target.value) )}/>
+        <p>Periodo</p><input type="number" value={periodo} onChange={e => setPeriodo( Number(e.target.value) )}/>
+
+        <button onClick={Juros}>Calcular</button>
+        {resposta}
 
     </div>
  )
+
+ }
+
---
+++ src/pages/media/index.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import './index.css'
+
+
+function listarAlunos(qtdAlunos) {
+    let array = [];
+
+    for(let i = 1; i <= qtdAlunos; i++){
+        array = [...array, i];
+
+    }
+
+        return array;
+}
+
+export default function MediaAlunos() {
+    const [alunos, setAlunos] = useState();
+    const [qtdAlunos, setQtdAlunos] = useState([]);
+    const [nota, setNota] = useState([])
+
+
+    function listarQtdAlunos() {
+        const resposta = listarAlunos(alunos);
+        console.log(resposta);
+        setQtdAlunos(resposta);
+    }
+
+    return (
+        <main>
+
+            <div>
+                Qtd.Alunos: <input type='number' value={alunos} onChange={e => setAlunos(Number(e.target.value))} />
+                <button onClick={listarQtdAlunos}>Ok</button>
+            </div>
+
+            {qtdAlunos.map(item =><div> Alunos: {item} <input /></div>
+            )}
+
+            <button >Calcular</button>
+
+        </main>
+    );
+}
